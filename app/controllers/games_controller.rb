580ce44ebff3de34b5ac755aa878a962602d634c
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-02-20 10:06:59 -0500

    word checker doesn't work

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,14 +11,28 @@ class GamesController < ApplicationController
 
   def score
     @word = params[:word]
+    # @letters = params[:letters]
+    @letters = ["c", "a", "l"]
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     word_serialized = open(url).read
     @word_spec = JSON.parse(word_serialized)
 
-    if @word_spec['found']
-      @reply = "Nice! '#{@word.capitalize}' is a valid english word that can be built out of #{@sample_letters}"
+    @word = "call"
+    @letters = ["a", "c", "l"]
+    @word_array = @word.split(//)
+    @word_array_const = @word.split(//)
+    @letters.each do |letter|
+      if @word_array.include?(letter)
+        @word_array.delete_at(0)
+      end
+    end
+
+    if @word_spec['found'] && word_check == true && counter == @word.length
+      @reply = "Nice! '#{@word.capitalize}' is a valid english word that can be built out of #{@letters}"
+    elsif @word_spec['found'] == false
+      @reply = "Boohoo.. '#{@word.capitalize}' is NOT english word"
     else
-      @reply = "Boohoo.. '#{@word.capitalize}' is NOT english"
+      @reply = "Boohoo.. '#{@word.capitalize}' is indeed a word, but cannot be built with #{@letters}"
     end
 
   end
